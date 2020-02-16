87001a56286abe0d61e7973deb1619370460b4c8
grifonna8 <grifonna8@gmail.com>
2020-02-16 21:18:41 +0300

    feat: made new ajax post request but it doesn't work properly

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   const requestURL1 = 'https://my-json-server.typicode.com/aero-frontend/test-task/PRODUCTS_SUCCESS';
-  const  xhr = new XMLHttpRequest();
-  xhr.open('GET', requestURL1);
+  const requestURL2 = 'https://my-json-server.typicode.com/aero-frontend/test-task/FAVORITE_SUCCESS';
+  const  xhr1 = new XMLHttpRequest();
+  xhr1.open('GET', requestURL1);
 
-  xhr.responseType = 'json';
-  xhr.onload = () => {
-    if (xhr.status !== 200 && xhr.readyState !== 4){
-      console.error(xhr.status);
+  xhr1.responseType = 'json';
+  xhr1.onload = () => {
+    if (xhr1.status !== 200 && xhr1.readyState !== 4){
+      console.error(xhr1.status);
     }
-    console.log(xhr.response);
+    console.log(xhr1.response);
+
+    let id = document.querySelectorAll('.card');
+    for (let i = 0; i < id.length; i++){
+      for(let j in xhr1.response.data.products){
+        if (i === +j){
+          id[i].setAttribute('id', xhr1.response.data.products[i].id);    
+        }
+      }
+    }
+
     let article = document.querySelectorAll('.card__article-number');
     article.forEach(item => {
-      for(let i in xhr.response.data.products){
-        item.innerText = 'Арт. ' + xhr.response.data.products[i].code;
+      for(let i in xhr1.response.data.products){
+        item.innerText = 'Арт. ' + xhr1.response.data.products[i].code;
       }
     });
 
     let check = document.querySelectorAll('.card__stock');
     check.forEach(item => {
-      for(let i in xhr.response.data.products){
-        if (xhr.response.data.products[i].availability === true){
+      for(let i in xhr1.response.data.products){
+        if (xhr1.response.data.products[i].availability === true){
           item.insertAdjacentHTML('beforeend', 'В наличии');
           break;
         }
@@ -28,80 +39,84 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     let title = document.querySelectorAll('.card__title');
     title.forEach(item => {
-      for(let i in xhr.response.data.products){
-        item.innerText = xhr.response.data.products[i].title;
+      for(let i in xhr1.response.data.products){
+        item.innerText = xhr1.response.data.products[i].title;
       }
     });
 
     let subtitle = document.querySelectorAll('.card__title');
     subtitle.forEach(item => {
-      for(let i in xhr.response.data.products){
-        for(let j = xhr.response.data.products[i].params.length - 1; j >= 0; j--){
+      for(let i in xhr1.response.data.products){
+        for(let j = xhr1.response.data.products[i].params.length - 1; j >= 0; j--){
           item.insertAdjacentHTML('afterend', '<div class="card__subtitle card__subtitle--first"><span class="info--first">' +
-           xhr.response.data.products[i].params[j].name +
-          '</span><span class="info--second">' + xhr.response.data.products[i].params[j].value + '</span></div>');
+           xhr1.response.data.products[i].params[j].name +
+          '</span><span class="info--second">' + xhr1.response.data.products[i].params[j].value + '</span></div>');
         }
         break;
       }
     });
 
     let fav = document.querySelectorAll('.fav');
-    console.log(fav);
     for (let i = 0; i < fav.length; i++){
-      for(let j in xhr.response.data.products){
-        if (xhr.response.data.products[j].inFav === true && i === +j){
+      for(let j in xhr1.response.data.products){
+        if (xhr1.response.data.products[j].inFav === true && i === +j){
           fav[i].setAttribute('style', 'background: url("../img/favourite-active.svg")');
         }
       }
     }
 
-    // let fav1 = document.querySelector('#fav1');
-    // for(let j in xhr.response.data.products){
-    //   if (xhr.response.data.products[j].inFav === true){
-    //     fav1.setAttribute('style', 'background: url("../img/favourite-active.svg")');
-    //   }
-    // }
-
-    // let fav2 = document.querySelector('#fav2');
-    // for(let j in xhr.response.data.products){
-    //   if (xhr.response.data.products[j].inFav === true){
-    //     fav2.setAttribute('style', 'background: url("../img/favourite-active.svg")');
-    //   }
-    // }
-
-    // let fav3 = document.querySelector('#fav3');
-    // for(let j in xhr.response.data.products){
-    //   if (xhr.response.data.products[j].inFav === true){
-    //     fav3.setAttribute('style', 'background: url("../img/favourite-active.svg")');
-    //   }
-    // }
-
-    xhr.response.data.products.forEach(item => {
-    /* const { id, code, imgUrl } = item; */
-    /* console.log( `ID=${id} ${code} ${imgUrl}`); */
-
-    // document.body.insertAdjacentHTML('beforeend',  
-    // `<div id="product-${item.id}">
-    //   <a href="${item.link}">
-    //     <h1>${item.title}</h1>
-    //     <h2>Code=${item.code}</h2>
-
-    //   </a>
-    //   <img src="${item.imgUrl}" alt="camera">
-    // </div>
-    // `);
-    });
   };
 
-  /* xhr.onerror = () => { */
-    /* в случае ошибки выведет то, что тут сделаем */
- /*  }; */
-
-  xhr.send();
-
-  
-
-  // 
-  // 
+  xhr1.send();
  
+
+  let favClick = document.querySelectorAll('.fav'),
+      card = document.querySelectorAll('.card'),
+      idMain;
+  
+  favClick.forEach(element => {
+    element.addEventListener('click', () => {
+      element.classList.add('active');
+      card.forEach(item => {
+        if (item.contains(element)){
+          idMain = item.getAttribute('id');
+          sendRequest2('POST', requestURL2, idMain)
+          .then(data => console.log(data))
+          .catch(err => console.log(err));
+        } else if (item.contains(element)){
+          idMain = item.getAttribute('id');
+          sendRequest2('POST', requestURL2, idMain)
+          .then(data => console.log(data))
+          .catch(err => console.log(err));
+        } else if (item.contains(element)){
+          idMain = item.getAttribute('id');
+          sendRequest2('POST', requestURL2, idMain)
+          .then(data => console.log(data))
+          .catch(err => console.log(err));
+        }
+      });
+      function sendRequest2(method, url, body) {
+        return new Promise((resolve, reject) => {
+          const  xhr2 = new XMLHttpRequest();
+          xhr2.open(method, url, body);
+          xhr2.responseType = 'json';
+          xhr2.setRequestHeader('Content-Type', 'application/json')
+          xhr2.onload = () => {
+            if (xhr2.status >= 400 && xhr2.readyState !== 4){
+              reject(xhr2.status);
+            }
+            resolve(xhr2.response);
+          };
+    
+          xhr2.onerror = () => {
+          reject(xhr2.response);
+          };
+    
+          xhr2.send(JSON.stringify(body)); 
+        });
+    
+      }
+    });
+  });
+  
 });
